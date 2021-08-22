331d1b26d44f4ee49f995695986bd416eb52cb23
adelawson <30274219+adelawson@users.noreply.github.com>
2021-08-22 14:53:55 +0100

    Added popup message and modified loading screen and status bar

--- app/src/main/java/com/example/hng/splashscreen.java
+++ app/src/main/java/com/example/hng/splashscreen.java
@@ -13,13 +13,10 @@ public class splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        splashHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(splashscreen.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        splashHandler.postDelayed(() -> {
+            Intent intent=new Intent(splashscreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
         },3000);
 
             }
